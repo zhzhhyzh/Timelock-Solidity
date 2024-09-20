bb0283de266ce721b5fac99e8823c0045bac4213
Zheheng <zheheng@33digitec.com>
2024-09-20 16:01:23 +0800

    Config set

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,26 +1,20 @@
 var TimelockContract = artifacts.require("Timelock");
 var AccountManagerContract = artifacts.require("AccountManager");
-var VoteAdminContract = artifacts.require("VoteAdmin")
 
 module.exports = async function (deployer) {
   // deployment steps
 
   let accManagerContractAddress = "";
   let TimelockContractAddress = "";
-  let voteAdminContractAddress = "";
   await deployer.deploy(AccountManagerContract).then(({ address }) => {
     console.log(address)
     accManagerContractAddress = address;
 
   });
-  if (accManagerContractAddress) await deployer.deploy(VoteAdminContract, accManagerContractAddress).then(({ address }) => {
-    console.log(address)
-    voteAdminContractAddress = address;
-
-  });
-  if (voteAdminContractAddress) await deployer.deploy(TimelockContract, accManagerContractAddress, voteAdminContractAddress).then(({ address }) => {
+  if (accManagerContractAddress) await deployer.deploy(TimelockContract, accManagerContractAddress, 10, 3600, 60).then(({ address }) => {
     console.log(address)
     TimelockContractAddress = address;
 
   });
+  
 };
