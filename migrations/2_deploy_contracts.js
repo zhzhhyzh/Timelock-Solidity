0c5adffddd6e16c8f95ca62814940f6b56a2bc0e
Zheheng <zheheng@33digitec.com>
2024-09-22 22:50:08 +0800

    Account manager final fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ var AccountManagerContract = artifacts.require("AccountManager");
 module.exports = async function (deployer) {
   // deployment steps
 
-  let accManagerContractAddress = "0x26aa06e10a8e1d6e5dd7dc20faab2be85b888e8a52df63c3e77bef3b75ca8cdf";
-  let TimelockContractAddress = "0x51f34b3d1dbc0a6ac1a0d2df96dae5bc6a62edb944b4ee2fb96591c5adef3c4e";
+  let accManagerContractAddress = "0x43bF796AA1f21996783418b9725A0EeB2523A7B1";
+  let TimelockContractAddress = "0x89dD744E9fe90286c4936100fc073b302a4cDCC2";
   await deployer.deploy(AccountManagerContract).then(({ address }) => {
     console.log(address)
     accManagerContractAddress = address;
