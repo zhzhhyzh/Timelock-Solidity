f3813a8ad3f7c060e6ab16b7e557a8eae3d56762
quintonpyx <quinton@33digitec.com>
2024-09-22 19:40:41 +0800

    navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -17,8 +17,8 @@ const showMenu = (toggleId, navbarId, bodyId)=>{
   /*===== LINK ACTIVE  =====*/ 
   const linkColor = document.querySelectorAll('.nav__link')
   function colorLink(){
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
+    // linkColor.forEach(l=> l.classList.remove('active'))
+    // this.classList.add('active')
   }
   linkColor.forEach(l=> l.addEventListener('click', colorLink))
   
